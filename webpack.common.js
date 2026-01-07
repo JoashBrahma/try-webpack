6f1879e1abd4c6b53601343e3a3d7ab0708a8f04
Joash Brahma <joashbrahma@zohomail.com>
2026-01-07 15:42:30 +0530

    webpack config files

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/index.html"]
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "./src/index.html" })
@@ -20,7 +15,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader" ,"css-loader"]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.html$/i,
